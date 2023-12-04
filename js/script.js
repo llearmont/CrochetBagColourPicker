1a013878a29575a02f4201d4acaa2e5faf51a740
l.learmont <l.learmont@uleth.ca>
2023-12-04 14:20:33 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,398 @@
+active_colour = "var(--BProbinEgg)";
+edging = false;
+
+$(".edge").hide();
+$("#colourSelector").hide();
+$("#colour-picker").css("background-color", active_colour);
+
+$(".yarn").click(function () {
+    $("#colourSelector").fadeToggle();
+});
+
+$(".edging").click(function () {
+    if (edging == false) {
+        $("#edge_toggle").text("On");
+        edging = true;
+    } else {
+        $("#edge_toggle").text("Off");
+        edging = false;
+    }
+    $(".edge").fadeToggle();
+});
+
+$(".burgundyButton").click(function () {
+    active_colour = "var(--BPburgundy)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".chiliButton").click(function () {
+    active_colour = "var(--BPchili)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".coralPeachButton").click(function () {
+    active_colour = "var(--BPcoralPeach)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".redButton").click(function () {
+    active_colour = "var(--BPred)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".cantaloupeButton").click(function () {
+    active_colour = "var(--BPcantaloupe)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".carrotButton").click(function () {
+    active_colour = "var(--BPcarrot)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".orangeButton").click(function () {
+    active_colour = "var(--BPorange)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".babyYellowButton").click(function () {
+    active_colour = "var(--BPbabyYellow)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".goldButton").click(function () {
+    active_colour = "var(--BPgold)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".goldenWheatButton").click(function () {
+    active_colour = "var(--BPgoldenWheat)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".yellowBalloonButton").click(function () {
+    active_colour = "var(--BPyellowBalloon)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".evergreenButton").click(function () {
+    active_colour = "var(--BPevergreen)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".greenButton").click(function () {
+    active_colour = "var(--BPgreen)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".ivyButton").click(function () {
+    active_colour = "var(--BPivy)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".mossButton").click(function () {
+    active_colour = "var(--BPmoss)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".paddyGreenButton").click(function () {
+    active_colour = "var(--BPpaddyGreen)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".pineButton").click(function () {
+    active_colour = "var(--BPpine)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".sageMistButton").click(function () {
+    active_colour = "var(--BPsageMist)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".springGreenButton").click(function () {
+    active_colour = "var(--BPspringGreen)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".babyAquaButton").click(function () {
+    active_colour = "var(--BPbabyAqua)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".botanyTealButton").click(function () {
+    active_colour = "var(--BPbotanyTeal)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".icedAquaButton").click(function () {
+    active_colour = "var(--BPicedAqua)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".peacockButton").click(function () {
+    active_colour = "var(--BPpeacock)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".robinEggButton").click(function () {
+    active_colour = "var(--BProbinEgg)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".tealButton").click(function () {
+    active_colour = "var(--BPteal)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".babyBlueButton").click(function () {
+    active_colour = "var(--BPbabyBlue)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".blueButton").click(function () {
+    active_colour = "var(--BPblue)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".navyButton").click(function () {
+    active_colour = "var(--BPnavy)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".royalBlueButton").click(function () {
+    active_colour = "var(--BProyalBlue)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".sapphireButton").click(function () {
+    active_colour = "var(--BPsapphire)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".shadowBlueButton").click(function () {
+    active_colour = "var(--BPshadowBlue)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".skyBlueButton").click(function () {
+    active_colour = "var(--BPskyBlue)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".stormBlueButton").click(function () {
+    active_colour = "var(--BPstormBlue)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".babyLilacButton").click(function () {
+    active_colour = "var(--BPbabyLilac)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".grandPurpleButton").click(function () {
+    active_colour = "var(--BPgrandPurple)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".grapeSodaButton").click(function () {
+    active_colour = "var(--BPgrapeSoda)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".lilacButton").click(function () {
+    active_colour = "var(--BPlilac)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".purpleButton").click(function () {
+    active_colour = "var(--BPpurple)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".purpleAshButton").click(function () {
+    active_colour = "var(--BPpurpleAsh)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".ultraVioletButton").click(function () {
+    active_colour = "var(--BPultraViolet)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".candyPinkButton").click(function () {
+    active_colour = "var(--BPcandyPink)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".dahliaButton").click(function () {
+    active_colour = "var(--BPdahlia)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".hotPinkButton").click(function () {
+    active_colour = "var(--BPhotPink)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".pinkButton").click(function () {
+    active_colour = "var(--BPpink)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".pinkMacaroonButton").click(function () {
+    active_colour = "var(--BPpinkMacaroon)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".prettyInPinkButton").click(function () {
+    active_colour = "var(--BPprettyInPink)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".roseButton").click(function () {
+    active_colour = "var(--BProse)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".almondButton").click(function () {
+    active_colour = "var(--BPalmond)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".aranButton").click(function () {
+    active_colour = "var(--BParan)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".burlapButton").click(function () {
+    active_colour = "var(--BPburlap)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".caramelButton").click(function () {
+    active_colour = "var(--BPcaramel)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".coffeeGroundsButton").click(function () {
+    active_colour = "var(--BPcoffeeGrounds)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".parchmentButton").click(function () {
+    active_colour = "var(--BPparchment)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".truffleButton").click(function () {
+    active_colour = "var(--BPtruffle)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".blackButton").click(function () {
+    active_colour = "var(--BPblack)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".darkGreyHeatherButton").click(function () {
+    active_colour = "var(--BPdarkGreyHeather)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".medGreyHeatherButton").click(function () {
+    active_colour = "var(--BPmedGreyHeather)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".skyGreyButton").click(function () {
+    active_colour = "var(--BPskyGrey)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".softGreyHeatherButton").click(function () {
+    active_colour = "var(--BPsoftGreyHeather)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".whiteButton").click(function () {
+    active_colour = "var(--BPwhite)";
+    $("#colour-picker").css("background-color", active_colour);
+    $("#colourSelector").hide();
+});
+
+$(".scale").click(function () {
+    $(".cls-2").css("fill", active_colour);
+});
+
+$(".edge").click(function () {
+    $(".cls-6").css("fill", active_colour);
+});
+
+$("#Drawstring").click(function () {
+    $(".cls-5").css("fill", active_colour);
+});
+
+$("#Bag").click(function () {
+    $(".cls-1").css("fill", active_colour);
+});
+
